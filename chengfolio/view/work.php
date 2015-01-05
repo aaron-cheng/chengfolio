d878f52964617bcb2958515d98014ed4893686df
Aaron Cheng <aaron@chengfolio.com>
2015-01-05 00:34:03 -0800

    work page/overall framework

---
+++ chengfolio/view/work.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aaron Cheng's Portfolio</title>
+	<meta name="description" content="Aaron Cheng is a multi award&#45;winning UI designer + Product designer, who has been commissioned to design user interface for several companies."/>
+	
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8"/>
+	<meta name="keywords" content="ui, ux, user experience, startup, interface, hci, graphic design, animation, portfolio, design, architecture, website, aaron cheng"/>
+	<meta name="generator" content="2.3.50.166"/>
+	<link rel="shortcut icon" href="img/favicon.ico"/>
+
+	<!-- open sans font -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet' type='text/css'>
+	<!-- Bootstrap css-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+	<!-- My css-->
+	<link href="../css/global.css" rel="stylesheet">
+</head>
+
+<body>
+	<nav class="container-fluid">
+		<div class="row">
+			<div class="col-md-3 col-md-offset-1 col-xs-10">
+				<a class="nav__logo" href="index.html">
+					<img src="../img/global/icon_logo.png" class="img-responsive" alt="Aaron Cheng's Portfolio">
+				</a>
+			</div>
+
+			<div class="col-xs-2 visible-xs visible-sm btn btn__menu_expand">
+				<img src="img/global/icon_expand_menu.png" class="img-responsive pull-right" alt="Menu Button">
+			</div>
+
+
+			<ul class="col-md-7 hidden-xs hidden-sm">
+				<li class="pull-right">		
+					<a class="nav__txt" href="googlemap.php" target="_top">Google Map Customizer</a>
+				</li>
+
+				<li class="pull-right">		
+					<a class="nav__txt" href="about.php" target="_top">About</a>
+				</li>
+
+				<li class="pull-right">
+					<a class="nav__txt " href="services.php">Works</a>
+				</li>
+
+				<li class="pull-right">		
+					<a class="nav__txt" href="technology.php" target="_top">Blog</a>
+				</li>
+			</ul>
+		</div>
+			<!-- Mobile Menu -->
+			<div class="row visible-xs visible-sm">
+				<div class="col-xs-12">
+					<ul class="row nav__menu--mobile">
+						<li class="col-xs-12 text-center">		
+							<a class="nav__txt" href="technology.html" target="_top">Technology</a>
+						</li>
+
+						<li class="col-xs-12 text-center">
+							<a class="nav__txt " href="services.html">Services</a>
+						</li>
+						
+						<li class="col-xs-12 text-center">		
+							<a class="nav__txt" href="how_it_works.html" target="_top">How it works</a>
+						</li>
+
+						<li class="col-xs-12 text-center">		
+							<a class="nav__txt" href="http://borderxlab.com/blog/" target="_top">Blog</a>
+						</li>
+
+						<li class="col-xs-12">		
+							<a class="btn btn--primary" href="contact.html" target="_top">Contact</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+	</nav>	
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="txt--title margin--section_top">Mobile Application Design</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-6 col-md-4 margin--element_top_xl" >
+				<a href="ivylla.php" target="_top">
+					<img src="../img/work/work_img_5th-ave.png" class="img-responsive center-block" alt="Ivylla Home Page">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+
+			<div class="col-sm-6 col-md-4 margin--element_top_xl" >
+				<a href="heymneu.php" target="_top">
+					<img src="../img/work/work_img_heymenu.png" class="img-responsive center-block" alt="HeyMenu Screenshot">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+
+			<div class="col-sm-6 col-md-4 margin--element_top_xl">
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_ambassador.png" class="img-responsive center-block" alt="Ambassador Screenshot">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+
+			<div class="col-sm-6 col-md-4 margin--element_top_xl" >
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_digital-closet.png" class="img-responsive center-block" alt="Digital Closet Screenshot">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="txt--title margin--section_top">Website Design</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-6 col-md-4 margin--element_top_xl" >
+				<a href="ivylla.php" target="_top">
+					<img src="../img/work/work_img_borderxlab.png" class="img-responsive center-block" alt="Ivylla Home Page">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+
+			<div class="col-sm-6 col-md-4 margin--element_top_xl" >
+				<a href="ivylla.php" target="_top">
+					<img src="../img/work/work_img_ivylla.png" class="img-responsive center-block" alt="Ivylla Home Page">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+
+			<div class="col-sm-6 col-md-4 margin--element_top_xl">
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_agilecredit.png" class="img-responsive center-block" alt="Agile Credit Screenshot">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+			
+			<div class="col-sm-6 col-md-4 margin--element_top_xl">
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_boutiquesui.png" class="img-responsive center-block" alt="Boutique Sui Screenshot">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+			
+			<div class="col-sm-6 col-md-4 margin--element_top_xl">
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_aerofs.png" class="img-responsive center-block" alt="AeroFS Screenshot">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="txt--title margin--section_top">Industrial Design</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-6 col-md-4 margin--element_top_xl" >
+				<a href="ivylla.php" target="_top">
+					<img src="../img/work/work_img_reclink.png" class="img-responsive center-block" alt="Ivylla Home Page">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+
+			<div class="col-sm-6 col-md-4 margin--element_top_xl" >
+				<a href="ivylla.php" target="_top">
+					<img src="../img/work/work_img_parkinghousing.png" class="img-responsive center-block" alt="Ivylla Home Page">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+
+			<div class="col-sm-6 col-md-4 margin--element_top_xl">
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_grabbymouse.png" class="img-responsive center-block" alt="Agile Credit Screenshot">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+			
+			<div class="col-sm-6 col-md-4 margin--element_top_xl">
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_unisextoilet.png" class="img-responsive center-block" alt="Boutique Sui Screenshot">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="txt--title margin--section_top">Architecture And Urban Planning</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-6 col-md-4 margin--element_top_xl" >
+				<a href="ivylla.php" target="_top">
+					<img src="../img/work/work_img_urban-weaving.png" class="img-responsive center-block" alt="Urban Weaving">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+
+			<div class="col-sm-6 col-md-4 margin--element_top_xl" >
+				<a href="ivylla.php" target="_top">
+					<img src="../img/work/work_img_floating-pavilion.png" class="img-responsive center-block" alt="Floating Pavilion">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+
+			<div class="col-sm-6 col-md-4 margin--element_top_xl">
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_energy-efficiency-vs-architectural-design.png" class="img-responsive center-block" alt="Energy Efficiency vs Architectural Design">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+			
+			<div class="col-sm-6 col-md-4 margin--element_top_xl">
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_high-density-residential-district.png" class="img-responsive center-block" alt="High-density Residential District">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+			
+			<div class="col-sm-6 col-md-4 margin--element_top_xl">
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_high-density-vs-low-density.png" class="img-responsive center-block" alt="High-density vs Low-density">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+			
+			<div class="col-sm-6 col-md-4 margin--element_top_xl">
+				<a href="ambassador.php" target="_top">
+					<img src="../img/work/work_img_water-town-revitalization.png" class="img-responsive center-block" alt="Water Town Revitalization">
+					<h2 class="text-center margin--element_top_xs">Ivylla Web Application</h2>
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<footer class="margin--section_top">
+		<div class="container-fluid">
+			<div class="row margin--section_top">
+				<div class="col-sm-7 col-sm-offset-1 col-md-5 col-md-offset-2 margin--element_bottom_xs">
+					<p class="footer__copyright">Copyright &#169 2015, Aaron Cheng's Portfolio, All rights reserved.</p>
+				</div>
+
+				<div class="col-xs-4 col-sm-1 margin--element_bottom_xs">
+					<a href="https://www.facebook.com/zhibin.cheng" target="_top">
+						<img src="../img/global/icon_facebook.png" class="img-responsive center-block" alt="Facebook Icon">
+					</a>
+				</div>
+
+				<div class="col-xs-4 col-sm-1 margin--element_bottom_xs">
+					<a href="https://www.linkedin.com/in/chengfolio" target="_top">
+						<img src="../img/global/icon_Linkedin.png" class="img-responsive center-block" alt="Linkedin Icon">
+					</a>
+				</div>
+
+				<div class="col-xs-4 col-sm-1 margin--element_bottom_xs">
+					<a href="https://twitter.com/ChengFolio" target="_top">
+						<img src="../img/global/icon_twitter.png" class="img-responsive center-block" alt="Twitter Icon">
+					</a>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- javascripts -->
+	<script src="js/jquery-1.8.2.min.js" type="text/javascript"></script> 
+	<script src="js/jquery.tap.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/javascript.js" type="text/javascript"></script> 
+</body>
+</html>
