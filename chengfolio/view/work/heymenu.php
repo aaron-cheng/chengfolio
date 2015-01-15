afa588db0155b90cd95c625fee23b11251c68dbc
Aaron Cheng <aaron@chengfolio.com>
2015-01-14 22:47:12 -0800

    finish heymenu layout

--- chengfolio/view/work/heymenu.php
+++ chengfolio/view/work/heymenu.php
@@ -48,12 +48,12 @@
 				</div>
 
 				<div class="row">
-					<div class="col-md-6 margin--element_top_lg" >						
+					<div class="col-md-5  margin--element_top_lg" >						
 						<img src="/img/work/heymenu/heymenu_menu_launch-img.png" class="img-responsive center-block" alt="Launch image">
 						<p class="txt--primary_on_light text-center margin--element_top_xs">Launch Image</p>
 					</div>
 
-					<div class="col-md-6 margin--element_top_lg" >					
+					<div class="col-md-5 col-md-offset-2 margin--element_top_lg" >					
 						<img src="/img/work/heymenu/heymenu_menu_login.png" class="img-responsive center-block" alt="Login page">
 						<p class="txt--primary_on_light text-center margin--element_top_xs">Login page</p>
 					</div>
@@ -62,6 +62,196 @@
 		</div>
 	</div>
 
+	<div class="container-fluid padding--element_lg">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="txt--subtitle">User Interface - Waitress</h2>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6 margin--element_top_sm" >
+						<p class="">This is part is for the servers to open/change tables, which is not visible for customers. Switching from menu to table-editing page requires passcode</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<div class="col-md-4 margin--element_top" >
+					<img src="/img/work/heymenu/heymenu_menu_select-table.png" class="img-responsive center-block" alt="Select Table Screenshot">
+				</div>
+
+				<div class="col-md-4 margin--element_top" >
+					<img src="/img/work/heymenu/heymenu_menu_number-guests.png" class="img-responsive center-block" alt="Enter number of guests window">
+				</div>
+
+				<div class="col-md-4 margin--element_top" >
+					<img src="/img/work/heymenu/heymenu_menu_password.png" class="img-responsive center-block" alt="Passcode popup window">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid padding--element_lg work__background--differentiator">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="txt--subtitle">Menu Display</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<div class="col-md-4 margin--element_top" >
+					<img src="/img/work/heymenu/heymenu_menu_list-view.png" class="img-responsive center-block" alt="List View Screenshot">
+					<p class="txt--primary_on_light text-center margin--element_top_xs">List View</p>
+				</div>
+
+				<div class="col-md-4 margin--element_top" >
+					<img src="/img/work/heymenu/heymenu_menu_grid-view.png" class="img-responsive center-block" alt="Grid View Screenshot">
+					<p class="txt--primary_on_light text-center margin--element_top_xs">Gird View</p>
+				</div>
+
+				<div class="col-md-4 margin--element_top" >
+					<img src="/img/work/heymenu/heymenu_menu_dish-detail.png" class="img-responsive center-block" alt="Dish Detail Screenshot">
+					<p class="txt--primary_on_light text-center margin--element_top_xs">Dish Detail</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid padding--element_lg">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="txt--subtitle">User Interface - Place Order</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-3 margin--element_top" >
+				<img src="/img/work/heymenu/heymenu_menu_confirm-order.png" class="img-responsive center-block" alt="View Order Screenshot">
+				<p class="txt--primary_on_light text-center margin--element_top_xs">View Order</p>
+			</div>
+
+			<div class="col-md-3 margin--element_top" >
+				<img src="/img/work/heymenu/heymenu_menu_placed-order.png" class="img-responsive center-block" alt="Screenshot after order is placed">
+				<p class="txt--primary_on_light text-center margin--element_top_xs">Order is Placed</p>
+			</div>
+
+			<div class="col-md-3 margin--element_top" >
+				<img src="/img/work/heymenu/heymenu_menu_new-order.png" class="img-responsive center-block" alt="Add New Order Screenshot">
+				<p class="txt--primary_on_light text-center margin--element_top_xs">Add New Order</p>
+			</div>
+
+			<div class="col-md-3 margin--element_top" >
+				<img src="/img/work/heymenu/heymenu_menu_new-order-expand.png" class="img-responsive center-block" alt="Show Previous Order">
+				<p class="txt--primary_on_light text-center margin--element_top_xs">Show Previous Order</p>
+			</div>			
+		</div>
+	</div>
+
+	<div class="container-fluid padding--element_lg work__background--differentiator">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="txt--subtitle">Admin Web Application - Order/Table Status</h2>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-6 margin--element_top_sm" >
+						<p class="">Regular users and admin users have different user interface: regular users can only manage daily operations, admin users have access to higher level settings.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10">
+				<div class="col-md-6 margin--element_top" >
+					<img src="/img/work/heymenu/heymenu_admin_order.jpg" class="img-responsive center-block img__wrapper" alt="What Regular User see in 'order' page.">
+					<p class="txt--primary_on_light text-center margin--element_top_xs">Regular User</p>
+				</div>
+
+				<div class="col-md-6 margin--element_top" >
+					<img src="/img/work/heymenu/heymenu_admin_order-admin.jpg" class="img-responsive center-block img__wrapper" alt="What Admin User see in 'order' page.">
+					<p class="txt--primary_on_light text-center margin--element_top_xs">Admin User</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid padding--element_lg">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="txt--subtitle">Menu</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="margin--element_top" >
+				<img src="/img/work/heymenu/heymenu_admin_menu.jpg" class="img-responsive center-block img__wrapper" alt="Dish Item List Screenshot">
+				<p class="txt--primary_on_light text-center margin--element_top_xs">Dish Item List</p>
+			</div>		
+		</div>
+
+		<div class="row">
+			<div class="margin--element_top" >
+				<img src="/img/work/heymenu/heymenu_admin_add-dish.jpg" class="img-responsive center-block img__wrapper" alt="Add Dish Item Screenshot">
+				<p class="txt--primary_on_light text-center margin--element_top_xs">Add Dish Item</p>
+			</div>		
+		</div>
+	</div>
+
+	<div class="container-fluid padding--element_lg work__background--differentiator">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">
+				<div class="row">
+					<div class="col-md-12">
+						<h2 class="txt--subtitle">Transaction</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-3 margin--element_top" >
+				<img src="/img/work/heymenu/heymenu_admin_transaction-summary.jpg" class="img-responsive center-block img__wrapper" alt="Summary Screenshot">
+				<p class="txt--primary_on_light text-center margin--element_top_xs">Summary</p>
+			</div>
+
+			<div class="col-md-3 margin--element_top" >
+				<img src="/img/work/heymenu/heymenu_admin_transaction-item.jpg" class="img-responsive center-block img__wrapper" alt="Transaction by item Screenshot">
+				<p class="txt--primary_on_light text-center margin--element_top_xs">Transaction by item</p>
+			</div>
+
+			<div class="col-md-3 margin--element_top" >
+				<img src="/img/work/heymenu/heymenu_admin_transaction-category.jpg" class="img-responsive center-block img__wrapper" alt="Transaction by category Screenshot">
+				<p class="txt--primary_on_light text-center margin--element_top_xs">Transaction by category</p>
+			</div>
+
+			<div class="col-md-3 margin--element_top" >
+				<img src="/img/work/heymenu/heymenu_admin_transaction-activities.jpg" class="img-responsive center-block img__wrapper" alt="Activities Screenshot">
+				<p class="txt--primary_on_light text-center margin--element_top_xs">Activities</p>
+			</div>			
+		</div>
+	</div>
 <?php include '../component/footer.php';?>
 
 <?php include '../component/script.php';?>
