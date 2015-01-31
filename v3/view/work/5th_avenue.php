9c2b47c8d05e7fa1256df08b95622e2c364693c4
Aaron Cheng <aaron@chengfolio.com>
2015-01-30 20:56:50 -0800

    update 5th ave alt txt

--- v3/view/work/5th_avenue.php
+++ v3/view/work/5th_avenue.php
@@ -16,11 +16,11 @@
 	<div class="container work__header__wrapper">
 		<div class="row">
 			<div class="col-md-3 col-md-offset-1 margin--element_top_lg">
-				<img src="/img/work/5th_avenue/5th_ave_logo.png" class="img-responsive center-block margin--element_top_lg" alt="HeyMenu Logo">
+				<img src="/img/work/5th_avenue/5th_ave_logo.png" class="img-responsive center-block margin--element_top_lg" alt="5th Avenue Logo">
 			</div>			
 
 			<div class="col-md-8 margin--element_top">
-				<img src="/img/work/work_img_5th-ave.png" class="img-responsive center-block" alt="HeyMenu Screenshot">
+				<img src="/img/work/work_img_5th-ave.png" class="img-responsive center-block" alt="5th Avenue Screenshot">
 			</div>
 		</div>
 
@@ -49,19 +49,19 @@
 
 					<div class="row">
 						<div class="col-md-8 col-md-offset-2 margin--element_top" >
-							<img src="/img/work/5th_avenue/5th_ave_logo_design.png" class="img-responsive center-block" alt="HeyMenu logo design process">
+							<img src="/img/work/5th_avenue/5th_ave_logo_design.png" class="img-responsive center-block" alt="5th Avenue logo design process">
 						</div>
 					</div>
 
 					<div class="row">
 						<div class="col-md-8 margin--element_top_sm" >
-							<p class="margin--element_top">The idea of the logo design comes from the APP’s name – ‘Fifth Avenue’. Considering the logo will be used as the APP icon on iphone, ‘5’ is designed to take the full height of the frame to make a strong impression. The upper case ‘TH’ matches the rectangular frame around.</p>
+							<p class="margin--element_top">The idea of the logo design comes from the APP’s name – ‘Fifth Avenue’. Considering the APP icon will be displayed on the small screen on iphone, ‘5’ is designed to take the full height of the frame to make a strong impression. The upper case ‘TH’ matches the rectangular frame around.</p>
 						</div>
 					</div>
 
 					<div class="row">
 						<div class="col-md-12 margin--element_top_lg" >						
-							<img src="/img/work/5th_avenue/5th_ave_photo.jpg" class="img-responsive center-block" alt="Launch image">
+							<img src="/img/work/5th_avenue/5th_ave_photo.jpg" class="img-responsive center-block" alt="5th Avenue Photo">
 						</div>
 					</div>
 				</div>
@@ -76,10 +76,10 @@
 					<div class="col-md-5">
 						<h2 class="txt--subtitle margin--element_top_lg">Introducing - 'One Page Display'</h2>
 						<p class="margin--element_top_sm">Users tend to switch back and forth frequently between category and items’ detail pages while doing window shopping on mobile platform. However, in most APPs, the ‘back’ button in the detail page is located on the top left corner, which makes it very difficult to reach. Also switching between different pages is not a very smooth user experience. </br></br> 
-						To solve those two problems, I came up with the ‘One Page Display’ UI. Once user clicks any item, the picture will be enlarged to take the same space as a typical detail page. The tab bar and navigation bar will move away, so the top and bottom of the category page is still visible and dimed as the background. To close the detail page, users can simply tap anywhere on the bottom part. The animation and layout make the user feel he/she can navigate without leave the category page. The intuitive interaction also makes the switching much easier and faster. </p>
+						To solve those two problems, I came up with the ‘One Page Display’ UI. Once user clicks any item, the picture will be enlarged to take the same space as a typical detail page. The tab bar and navigation bar will move away, but the top and bottom of the category page is still visible and dimed as the background. To close the detail page, users can simply tap anywhere on the bottom part. The animation and layout make the user feel he/she can navigate without leaving the category page. The intuitive interaction makes the switching much easier and faster. </p>
 					</div>
 					<div class="col-md-6 col-md-offset-1">
-						<img src="/img/work/5th_avenue/5th_ave_one_page_display.gif" class="img-responsive center-block margin--element_top_lg" alt="Passcode popup window">
+						<img src="/img/work/5th_avenue/5th_ave_one_page_display.gif" class="img-responsive center-block margin--element_top_lg" alt="One page display design animation">
 					</div>					
 				</div>
 			</div>
@@ -98,7 +98,7 @@
 
 					<div class="row">
 						<div class="col-md-12 margin--element_top_lg" >
-							<img src="/img/work/5th_avenue/5th_ave_tab_bar.png" class="img-responsive center-block" alt="Select Table Screenshot">
+							<img src="/img/work/5th_avenue/5th_ave_tab_bar.png" class="img-responsive center-block" alt="Tabbar design">
 						</div>				
 					</div>
 				</div>
@@ -120,36 +120,36 @@
 
 					<div class="row">
 						<div class="col-sm-6 col-md-5 margin--element_top_lg" >
-							<img src="/img/work/5th_avenue/5th_ave_category_lvl1.png" class="img-responsive center-block" alt="Select Table Screenshot">
+							<img src="/img/work/5th_avenue/5th_ave_category_lvl1.png" class="img-responsive center-block" alt="Level 1 category">
 							<p class="txt--primary_on_light text-center margin--element_top_xs">Level 1 category - All</p>
 						</div>
 
 						<div class="col-sm-6 col-md-5 col-md-offset-2 margin--element_top_lg" >
-							<img src="/img/work/5th_avenue/5th_ave_category_lvl1_expand.png" class="img-responsive center-block" alt="Enter number of guests window">
+							<img src="/img/work/5th_avenue/5th_ave_category_lvl1_expand.png" class="img-responsive center-block" alt="Expanded level 1 category list">
 							<p class="txt--primary_on_light text-center margin--element_top_xs">Men, Women, Accessory and etc</p>
 						</div>					
 					</div>
 
 					<div class="row">
 						<div class="col-sm-6 col-md-5 margin--element_top_lg" >
-							<img src="/img/work/5th_avenue/5th_ave_category_lvl2.png" class="img-responsive center-block" alt="Passcode popup window">
+							<img src="/img/work/5th_avenue/5th_ave_category_lvl2.png" class="img-responsive center-block" alt="Level 2 category">
 							<p class="txt--primary_on_light text-center margin--element_top_xs">Level 2 category - Women</p>
 						</div>
 
 						<div class="col-sm-6 col-md-5 col-md-offset-2 margin--element_top_lg" >
-							<img src="/img/work/5th_avenue/5th_ave_category_lvl2_expand.png" class="img-responsive center-block" alt="Select Table Screenshot">
+							<img src="/img/work/5th_avenue/5th_ave_category_lvl2_expand.png" class="img-responsive center-block" alt="Expanded level 2 category list">
 							<p class="txt--primary_on_light text-center margin--element_top_xs">Top, Bottom, Bag and etc</p>
 						</div>					
 					</div>
 
 					<div class="row">
 						<div class="col-sm-6 col-md-5 margin--element_top_lg" >
-							<img src="/img/work/5th_avenue/5th_ave_category_lvl3.png" class="img-responsive center-block" alt="Enter number of guests window">
+							<img src="/img/work/5th_avenue/5th_ave_category_lvl3.png" class="img-responsive center-block" alt="Level 3 category">
 							<p class="txt--primary_on_light text-center margin--element_top_xs">Level 3 category - Top</p>
 						</div>
 
 						<div class="col-sm-6 col-md-5 col-md-offset-2 margin--element_top_lg" >
-							<img src="/img/work/5th_avenue/5th_ave_category_lvl3_expand.png" class="img-responsive center-block" alt="Passcode popup window">
+							<img src="/img/work/5th_avenue/5th_ave_category_lvl3_expand.png" class="img-responsive center-block" alt="Expanded level 3 category list">
 							<p class="txt--primary_on_light text-center margin--element_top_xs">Shirts, Sleeveless Tops, Blouse and etc</p>
 						</div>					
 					</div>
@@ -169,10 +169,10 @@
 				</div>
 				<div class="row">
 					<div class="col-sm-5 margin--element_top_lg">
-						<img src="/img/work/5th_avenue/5th_ave_filter.png" class="img-responsive center-block" alt="Passcode popup window">
+						<img src="/img/work/5th_avenue/5th_ave_filter.png" class="img-responsive center-block" alt="Filter Design">
 					</div>	
 					<div class="col-sm-5 col-sm-offset-2 margin--element_top_lg">
-						<img src="/img/work/5th_avenue/5th_ave_sort.png" class="img-responsive center-block" alt="Passcode popup window">
+						<img src="/img/work/5th_avenue/5th_ave_sort.png" class="img-responsive center-block" alt="Filter Design">
 					</div>												
 				</div>
 			</div>
@@ -191,7 +191,7 @@
 				<div class="row">
 					<div class="col-md-6 col-md-offset-3 margin--element_top_lg">
 						<div class="img__wrapper">
-							<img src="/img/work/5th_avenue/5th_ave_detail_full.jpg" class="img-responsive center-block margin--element_top_sm" alt="Passcode popup window">
+							<img src="/img/work/5th_avenue/5th_ave_detail_full.jpg" class="img-responsive center-block margin--element_top_sm" alt="Item detail page">
 						</div>
 					</div>
 				</div>
@@ -223,7 +223,7 @@
 					<div class="col-sm-4">
 						<div class="row">
 							<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-								<img src="/img/work/5th_avenue/5th_ave_brand.png" class="img-responsive center-block margin--element_top_lg" alt="Passcode popup window">
+								<img src="/img/work/5th_avenue/5th_ave_brand.png" class="img-responsive center-block margin--element_top_lg" alt="Brand list design">
 								<p class="txt--primary_on_light text-center margin--element_top_xs">Brand list</p>							
 							</div>
 						</div>
@@ -232,7 +232,7 @@
 					<div class="col-sm-4">
 						<div class="row">
 							<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-								<img src="/img/work/5th_avenue/5th_ave_brand_info.png" class="img-responsive center-block margin--element_top_lg" alt="Passcode popup window">
+								<img src="/img/work/5th_avenue/5th_ave_brand_info.png" class="img-responsive center-block margin--element_top_lg" alt="Brand introduction">
 								<p class="txt--primary_on_light text-center margin--element_top_xs">Brand introduction and news</p>								
 							</div>
 						</div>
@@ -241,7 +241,7 @@
 					<div class="col-sm-4">
 						<div class="row">
 							<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-								<img src="/img/work/5th_avenue/5th_ave_brand_item.png" class="img-responsive center-block margin--element_top_lg" alt="Passcode popup window">
+								<img src="/img/work/5th_avenue/5th_ave_brand_item.png" class="img-responsive center-block margin--element_top_lg" alt="Brand product list">
 								<p class="txt--primary_on_light text-center margin--element_top_xs">Brand products</p>							
 							</div>
 						</div>
@@ -272,7 +272,7 @@
 					<div class="col-sm-4 margin--element_top_lg">
 						<div class="row">
 							<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-								<img src="/img/work/5th_avenue/5th_ave_login.png" class="img-responsive center-block" alt="Passcode popup window">
+								<img src="/img/work/5th_avenue/5th_ave_login.png" class="img-responsive center-block" alt="login page">
 								<p class="txt--primary_on_light text-center margin--element_top_xs">Login</p>							
 							</div>
 						</div>
@@ -281,7 +281,7 @@
 					<div class="col-sm-4 margin--element_top_lg">
 						<div class="row">
 							<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-								<img src="/img/work/5th_avenue/5th_ave_signup.png" class="img-responsive center-block" alt="Passcode popup window">
+								<img src="/img/work/5th_avenue/5th_ave_signup.png" class="img-responsive center-block" alt="signup page">
 								<p class="txt--primary_on_light text-center margin--element_top_xs">Signup</p>								
 							</div>
 						</div>
@@ -290,7 +290,7 @@
 					<div class="col-sm-4 margin--element_top_lg">
 						<div class="row">
 							<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-								<img src="/img/work/5th_avenue/5th_ave_setting_signup.png" class="img-responsive center-block" alt="Passcode popup window">
+								<img src="/img/work/5th_avenue/5th_ave_setting_signup.png" class="img-responsive center-block" alt="Preference setting">
 								<p class="txt--primary_on_light text-center margin--element_top_xs">Preference Setting</p>							
 							</div>
 						</div>
@@ -319,7 +319,7 @@
 					<div class="col-sm-4 margin--element_top_lg">
 						<div class="row">
 							<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-								<img src="/img/work/5th_avenue/5th_ave_my_favorite.png" class="img-responsive center-block" alt="Passcode popup window">
+								<img src="/img/work/5th_avenue/5th_ave_my_favorite.png" class="img-responsive center-block" alt="My favorite products list">
 								<p class="txt--primary_on_light text-center margin--element_top_xs">My favorite products</p>							
 							</div>
 						</div>
@@ -328,7 +328,7 @@
 					<div class="col-sm-4 margin--element_top_lg">
 						<div class="row">
 							<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-								<img src="/img/work/5th_avenue/5th_ave_my_brand.png" class="img-responsive center-block" alt="Passcode popup window">
+								<img src="/img/work/5th_avenue/5th_ave_my_brand.png" class="img-responsive center-block" alt="My favorite brands list">
 								<p class="txt--primary_on_light text-center margin--element_top_xs">My favorite brands</p>								
 							</div>
 						</div>
@@ -337,7 +337,7 @@
 					<div class="col-sm-4 margin--element_top_lg">
 						<div class="row">
 							<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-								<img src="/img/work/5th_avenue/5th_ave_setting.png" class="img-responsive center-block" alt="Passcode popup window">
+								<img src="/img/work/5th_avenue/5th_ave_setting.png" class="img-responsive center-block" alt="Setting page">
 								<p class="txt--primary_on_light text-center margin--element_top_xs">Settings</p>							
 							</div>
 						</div>
@@ -371,7 +371,7 @@
 						<div class="col-sm-4 margin--element_top_lg">
 							<div class="row">
 								<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-									<img src="/img/work/5th_avenue/5th_ave_shopping_cart_1.png" class="img-responsive center-block" alt="Passcode popup window">
+									<img src="/img/work/5th_avenue/5th_ave_shopping_cart_1.png" class="img-responsive center-block" alt="Shopping cart">
 									<p class="txt--primary_on_light text-center margin--element_top_xs">Total price is unavailable without address</p>							
 								</div>
 							</div>
@@ -380,7 +380,7 @@
 						<div class="col-sm-4 margin--element_top_lg">
 							<div class="row">
 								<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-									<img src="/img/work/5th_avenue/5th_ave_shopping_cart_2.png" class="img-responsive center-block" alt="Passcode popup window">
+									<img src="/img/work/5th_avenue/5th_ave_shopping_cart_2.png" class="img-responsive center-block" alt="Shopping cart - choose address">
 									<p class="txt--primary_on_light text-center margin--element_top_xs">Required to choose address when select items</p>								
 								</div>
 							</div>
@@ -389,7 +389,7 @@
 						<div class="col-sm-4 margin--element_top_lg">
 							<div class="row">
 								<div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
-									<img src="/img/work/5th_avenue/5th_ave_shopping_cart_3.png" class="img-responsive center-block" alt="Passcode popup window">
+									<img src="/img/work/5th_avenue/5th_ave_shopping_cart_3.png" class="img-responsive center-block" alt="Shopping cart - updated price">
 									<p class="txt--primary_on_light text-center margin--element_top_xs">Shipping fee is added to total price</p>							
 								</div>
 							</div>
@@ -419,7 +419,7 @@
 	</div>
 	<div class="container-fluid work__background--differentiator padding--element_bottom_lg">
 		<div class="row">
-			<img src="/img/work/5th_avenue/5th_ave_shopping_cart_web.png" class="img-responsive center-block margin--element_top_sm" alt="Passcode popup window">
+			<img src="/img/work/5th_avenue/5th_ave_shopping_cart_web.png" class="img-responsive center-block margin--element_top_sm" alt=" exported shopping cart on web">
 		</div>
 	</div>
 
